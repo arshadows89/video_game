84856734cdcb745356a80b966c5246386ce709f1
Master <subtle3ntropy@gmail.com>
2015-01-23 18:20:55 -0500

    initial commit

--- main.rb
+++ main.rb
@@ -4,9 +4,11 @@ require 'mandrill'
 get '/' do
     erb :home
 end
+
 get '/story' do
 	erb :story
 end
+
 get '/classes' do
 	erb :classes
 end
@@ -14,3 +16,19 @@ end
 get '/pre_order' do
 	erb :pre_order
 end
+
+post '/contact' do
+  # puts "my params are: " + params.inspect
+  contact_name = params['name']
+  contact_email = params['email']
+  email_body = "We've received your payment"
+  m = Mandrill::API.new
+
+  sending = m.messages.send message  
+  puts sending
+  redirect to('/thanks')
+end
+
+ get '/thanks' do
+ 	erb :thanks
+ end
