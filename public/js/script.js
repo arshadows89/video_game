7ca436d4dd88e51d223878cf30fa3062f4f3a204
Cameron Snyder <Dooffycom@gmail.com>
2015-01-22 20:55:19 -0500

    added content to story page with tweaked js and js gems and basic temp css

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,39 @@
-    $(function(){
-      $("#slides").slidesjs({
-        width: 940,
-        height: 528
-      });
-    });
+$(function(){
+  $("#slides").slidesjs({
+
+    play: {
+      active: true,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry.
+      effect: "slide",
+        // [string] Can be either "slide" or "fade".
+      interval: 5000,
+        // [number] Time spent on each slide in milliseconds.
+      auto: true,
+        // [boolean] Start playing the slideshow on load.
+      swap: true,
+        // [boolean] show/hide stop and play buttons
+      pauseOnHover: false,
+        // [boolean] pause a playing slideshow on hover
+      restartDelay: 2500
+        // [number] restart delay on inactive slideshow
+    }
+  });
+});
+$(function(){
+  $("#slides").slidesjs({
+    pagination: {
+      active: false,
+        // [boolean] Create pagination items.
+        // You cannot use your own pagination. Sorry.
+      effect: "slide"
+        // [string] Can be either "slide" or "fade".
+    }
+  });
+});
+$(function(){
+  $("#slides").slidesjs({
+    width: 200,
+    height: 993
+  });
+});
